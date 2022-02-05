cd41ff8a0775c721d8182e2eedd63044c76ebca8
insert-username <insert-username@users.noreply.github.com>
2022-02-05 16:01:57 +0100

    Initial Commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="shapely_art_tools",
+    version="0.0.1",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
